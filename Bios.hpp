2806c8884edeb4c55c00bd2e3dbc69efcc53f6ec
williamblair <bblair12876@aol.com>
2017-11-15 10:17:16 -0500

    Update Bios.hpp

--- Bios.hpp
+++ Bios.hpp
@@ -1,9 +1,14 @@
 /* Bios class for psx emulator */
 
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 #ifndef BIOS_H_INCLUDED
 #define BIOS_H_INCLUDED
+
+// Bios images are always 512 kb in length
+const int BIOS_FILE_SIZE = 512 * 1024;
 
 class Bios
 {
@@ -12,7 +17,7 @@ public:
     // args: file path of the bios file
     Bios(const string &fPath);
 private:
-    std::vector<uint64_t> data;
+    std::vector<uint8_t> data;
 };
 
 #endif
